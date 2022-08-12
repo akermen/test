40037cda9529d28cbbe45f26e080ce0236916b04
akermen <akermen@gmail.com>
2022-08-12 10:38:36 +0300

    initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+now = datetime.now()
+
+# odev: strftime tarih ve saat format kisaltmalari
+# yil-ay-gun-saat-dakika-saniye.jpg
+file_name = now.strftime('%Y-%m')
+
+with open(file_name + '.txt', "w+") as f:
+    f.write("deneme")
+
+print(file_name)
+print('done')
